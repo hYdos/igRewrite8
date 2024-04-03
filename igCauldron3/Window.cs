53666801136f51c5318f9ba1eb8661450e3d7180
NefariousTechSupport <64808609+NefariousTechSupport@users.noreply.github.com>
2024-04-04 00:28:30 +0100

    removed escape key to close window

--- igCauldron3/Window.cs
+++ igCauldron3/Window.cs
@@ -43,7 +43,7 @@ namespace igCauldron3
 		{
 			if (KeyboardState.IsKeyDown(OpenTK.Windowing.GraphicsLibraryFramework.Keys.Escape))
 			{
-				Close();
+				//Close();
 			}
 
 			base.OnUpdateFrame(e);
