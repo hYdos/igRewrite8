904657bec0dde050e67dd9b8cf1ccb660bb449b8
NefariousTechSupport <Jaskaran7852@gmail.com>
2024-08-25 18:33:16 +0100

    Refactor igMetaEnum.cs

--- igLibrary/Core/igMetaEnum.cs
+++ igLibrary/Core/igMetaEnum.cs
@@ -5,14 +5,47 @@ using System.Reflection.Emit;
 
 namespace igLibrary.Core
 {
+	/// <summary>
+	/// Reflection metadata for an enum
+	/// </summary>
 	public class igMetaEnum : igBaseMeta
 	{
+		/// <summary>
+		/// No clue
+		/// </summary>
 		public bool _flags;
+
+
+		/// <summary>
+		/// The names of all the enum members
+		/// </summary>
 		public List<string> _names = new List<string>();
+
+
+		/// <summary>
+		/// The integer values of all the enum members
+		/// </summary>
 		public List<int> _values = new List<int>();
+
+
+		/// <summary>
+		/// The attributes of this igMetaEnum
+		/// </summary>
 		public igObject _attributes;
+
+
+		/// <summary>
+		/// The attributes of the members of this igMetaEnum
+		/// </summary>
 		public igObject _valueAttributes;
 
+
+
+
+
+		/// <summary>
+		/// Called after reading the igMetaEnum from the igArkCoreFile
+		/// </summary>
 		public override void PostUndump()
 		{
 			Type? t = igArkCore.GetEnumDotNetType(_name);
@@ -34,18 +67,34 @@ namespace igLibrary.Core
 				_internalType = t;
 			}
 		}
+
+
+		/// <summary>
+		/// Get the DotNet enum from the integer value
+		/// </summary>
+		/// <param name="value">The integer value</param>
+		/// <returns>The enum member as its DotNet type</returns>
+		/// <exception cref="NotImplementedException">Exception thrown when there is no associated generated type for this enum</exception>
 		public object GetEnumFromValue(int value)
 		{
 			if(_internalType == null) throw new NotImplementedException("this enum is not connected to any type. This feature will be implemented in the future");
 
 			int index = _values.IndexOf(value);
-			if(index < 0) //throw new KeyNotFoundException($"Value {value} not found in enum {_name}");
+			if(index < 0)
 			{
 				return Activator.CreateInstance(_internalType);
 			}
 
 			return Enum.Parse(_internalType, _names[index]);
 		}
+
+
+		/// <summary>
+		/// Get the integer value from the DotNet enum member
+		/// </summary>
+		/// <param name="enumValue">The DotNet enum member</param>
+		/// <returns>The integer value</returns>
+		/// <exception cref="NotImplementedException">Exception thrown when there is no associated generated type for this enum</exception>
 		public int GetValueFromEnum(object enumValue)
 		{
 			if(_internalType == null) throw new NotImplementedException("this enum is not connected to any type. This feature will be implemented in the future");
@@ -55,6 +104,14 @@ namespace igLibrary.Core
 
 			return _values[index];
 		}
+
+
+		/// <summary>
+		/// Get the DotNet enum member from its string name
+		/// </summary>
+		/// <param name="name">The name of the DotNet enum member</param>
+		/// <returns>The enum member as its DotNet type</returns>
+		/// <exception cref="NotImplementedException">Exception thrown when there is no associated generated type for this enum</exception>
 		public object GetEnumFromName(string name)
 		{
 			if(_internalType == null) throw new NotImplementedException("this enum is not connected to any type. This feature will be implemented in the future");
