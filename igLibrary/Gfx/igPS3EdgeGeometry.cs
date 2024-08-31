89cc6f043b0c8884ae0074dca5dd61f0de052e3f
NefariousTechSupport <Jaskaran7852@gmail.com>
2024-09-01 00:14:19 +0100

    Added ps3 edge geometry stuff

---
+++ igLibrary/Gfx/igPS3EdgeGeometry.cs
@@ -0,0 +1,11 @@
+namespace igLibrary.Gfx
+{
+	public class igPS3EdgeGeometry : igPS3EdgeGeometrySegmentList
+	{
+		public bool _isMorphed;
+		public bool _isSkinned;
+		public bool _isSpeedTree;
+		public bool _isVertexAnimated;
+		public bool _hasVertexColor;
+	}
+}
